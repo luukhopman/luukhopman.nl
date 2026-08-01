bfe894deded7daf300420972d7dbb7b827247787
Luuk Hopman <luukhopman@users.noreply.github.com>
2026-08-01 17:40:31 +0000

    Refresh Zusammen house app icon

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "nl.luukhopman.household"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 5
+        versionName = "1.4.0"
     }
 
     buildTypes {
